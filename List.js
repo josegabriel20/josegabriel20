bdc0443fecec261d5f0392f4fe54c5473254ad8d
josegabriel20 <josegabrielbermudez2007@gmail.com>
2026-06-18 15:47:41 +0000

    subiendo archivos

---
+++ List.js
@@ -0,0 +1,5 @@
+const List = [
+    //  {titulo: "Estrella de Plata", lineas:"Por la noche aparece un hombre misterioso, por la mañana un asesinato, la cabeza del difunto gravemente golpeada y un pañuelo que pertenecia al hombre de la noche anterior en su mano¿Serás capaz de resolver el misterio?", image:false,ruta: "estrelladeplata",lang:"es",left:"linea1"},
+      {titulo:"Despues de Veinte Años",lineas:"Dos viejos amigos deciden ir y hacer una fortuna cada uno por su cuenta y re-encontrarse en el mismo lugar despues de veinte años", image:false,ruta: "despuesdeveinteanos",lang:"es",left:"linea4"},
+          {titulo:"El Reloj",lineas:"Un hombre rico muy celoso pierde su preciado reloj muy costoso una herencia familiar luego descubre que...", image:false,ruta: "elreloj",lang:"es",left:"linea5"},
+            ];
